64ab979e738904641175e755aa8d5ca6beeb5f9b
Martin Blažek <martinblazek6@gmail.com>
2021-10-09 13:33:14 +0200

    Commit feature try

---
+++ src/sum_all.java
@@ -0,0 +1,13 @@
+public class sum_all {
+    public static void main(String[] args){
+        // - Create an array variable named `numbers`
+        //   with the following content: `[3, 4, 5, 6, 7]`
+        // - Print the sum of the elements of `numbers`
+        int sum=0;
+        int[] numbers = {3, 4, 5, 6, 7};
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i];
+        }
+        System.out.println(sum);
+    }
+}
